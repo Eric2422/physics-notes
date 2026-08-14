268c9bc0231f178232c23fca8f9a253d6aa067f0
Eric Zhang <ericbkzhang@gmail.com>
2026-08-14 13:03:03 -0400

    Insert velocity graph

--- vol1-mechanics/src/drag.py
+++ vol1-mechanics/src/drag.py
@@ -1,22 +1,44 @@
+from pathlib import Path
+
 import matplotlib.pyplot as plt
 import numpy as np
 import numpy.typing as npt
 
-GRAVITY = 9.80665
-DRAG_COEFFICIENT = 1
-MASS = 1
 
-terminal_velocity = MASS / DRAG_COEFFICIENT * GRAVITY
+def velocity(
+    time: npt.NDArray[np.float64],
+    gravity: float = 9.80665,
+    drag_coefficient: float = 0,
+    mass: float = 1.0,
+    initial_velocity: float = 0
+) -> npt.NDArray:
+    """Return the velocity of a falling object at the given times.
 
+    Parameters
+    ----------
+    initial_velocity : `float`
+        The initial velocity of the falling object in meters per second (m/s).
+    time : `npt.NDArray[np.float64]`
+        The times to calculate the velocities at in seconds (s).
 
-def velocity(initial_velocity: float, time: npt.NDArray[np.float64]) -> npt.NDArray:
+    Returns
+    -------
+    `npt.NDArray`
+        An array containing the velocities at each time in metres per second (m/s).
+    """
+    terminal_velocity = - mass / drag_coefficient * gravity
+
     return terminal_velocity + \
-        (terminal_velocity - initial_velocity) * \
-        np.exp(- DRAG_COEFFICIENT / MASS * time)
+        (initial_velocity - terminal_velocity) * \
+        np.exp(- drag_coefficient / mass * time)
 
 
 if __name__ == "__main__":
     time_domain = np.linspace(0, 10)
 
-    plt.plot(time_domain, velocity(0, time_domain))
+    plt.title("Velocity vs Time")
+    plt.xlabel("Time (s)")
+    plt.ylabel("Velocity (m/s)")
+    plt.plot(time_domain, velocity(time_domain, drag_coefficient=1.0))
+    plt.savefig(Path("../assets/drag-velocity.pgf"))
     plt.show()
